e01f5516778253c5251333b57c8c0baaacc954cf
Neeraj Singh Junior <NeerajSingh511997@gmail.com>
2020-04-21 19:16:24 +0530

    update miniChatBox_Project directory

---
+++ WebProjects/miniChatBox_Project/tokenDash/bootstrap/cache/packages.php
@@ -0,0 +1,99 @@
+<?php return array (
+  'acacha/admin-lte-template-laravel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Acacha\\AdminLTETemplateLaravel\\Providers\\AdminLTETemplateServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'AdminLTE' => 'Acacha\\AdminLTETemplateLaravel\\Facades\\AdminLTE',
+    ),
+  ),
+  'creativeorange/gravatar' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Creativeorange\\Gravatar\\GravatarServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Gravatar' => 'Creativeorange\\Gravatar\\Facades\\Gravatar',
+    ),
+  ),
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'hesto/multi-auth' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Hesto\\MultiAuth\\MultiAuthServiceProvider',
+    ),
+  ),
+  'jrean/laravel-user-verification' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Jrean\\UserVerification\\UserVerificationServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'UserVerification' => 'Jrean\\UserVerification\\Facades\\UserVerification',
+    ),
+  ),
+  'laravel/dusk' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Dusk\\DuskServiceProvider',
+    ),
+  ),
+  'laravel/passport' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Passport\\PassportServiceProvider',
+    ),
+  ),
+  'laravel/socialite' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Socialite\\SocialiteServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Socialite' => 'Laravel\\Socialite\\Facades\\Socialite',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'pragmarx/google2fa-laravel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'PragmaRX\\Google2FALaravel\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Google2FA' => 'PragmaRX\\Google2FALaravel\\Facade',
+    ),
+  ),
+);
